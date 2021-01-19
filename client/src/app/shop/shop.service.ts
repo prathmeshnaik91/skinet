b834298f7fa347430b23423228f89ffa58d5a095
Prathmesh Naik <prathmeshnaik91@gmail.com>
2021-01-19 10:55:18 +0530

    End of section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import {map} from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 
 @Injectable({
@@ -35,6 +36,9 @@ baseUrl='https://localhost:5001/api/'
         return response.body;
       })
     )
+  }
+  getProduct(id:number){
+    return this.http.get<IProduct>(this.baseUrl+'products/'+id);
   }
   getBrands(){
     return this.http.get<IBrand[]>(this.baseUrl+'products/brands')
